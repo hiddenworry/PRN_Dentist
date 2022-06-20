1aafd5510c6790ac5a8d7867c872b57109a602e8
ADMIN <hiddenwory@gmail.com>
2022-06-20 23:53:11 +0700

    first commit

---
+++ frmLogin/frmHome.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinApp
+{
+    public partial class frmHome : Form
+    {
+        List<Panel> panelList = new List<Panel>();
+        int index;
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAppointment_Click(object sender, EventArgs e)
+        {
+            panelCustomer.SendToBack();
+            panelService.SendToBack();
+            panelDentist.SendToBack();
+            panelAppointment.BringToFront();
+            buttonAppointment.BackColor = Color.LightBlue;
+            buttonDentist.BackColor = Color.Lavender;
+            buttonService.BackColor = Color.Lavender;
+            buttonCustomer.BackColor = Color.Lavender;
+        }
+
+        private void buttonCustomer_Click(object sender, EventArgs e)
+        {
+            panelCustomer.BringToFront();
+            panelService.SendToBack();
+            panelDentist.SendToBack();
+            panelAppointment.SendToBack();
+            buttonAppointment.BackColor = Color.Lavender;
+            buttonDentist.BackColor = Color.Lavender;
+            buttonService.BackColor = Color.Lavender;
+            buttonCustomer.BackColor = Color.LightBlue;
+        }
+
+        private void buttonService_Click(object sender, EventArgs e)
+        {
+            panelDentist.SendToBack();
+            panelAppointment.SendToBack();
+            panelCustomer.SendToBack();
+            panelService.BringToFront();
+            buttonAppointment.BackColor = Color.Lavender;
+            buttonDentist.BackColor = Color.Lavender;
+            buttonService.BackColor = Color.LightBlue;
+            buttonCustomer.BackColor = Color.Lavender;
+        }
+
+        private void buttonDentist_Click(object sender, EventArgs e)
+        {
+            panelAppointment.SendToBack();
+            panelCustomer.SendToBack();
+            panelService.SendToBack();
+            panelDentist.BringToFront();
+            buttonAppointment.BackColor = Color.Lavender;
+            buttonDentist.BackColor = Color.LightBlue;
+            buttonService.BackColor = Color.Lavender;
+            buttonCustomer.BackColor = Color.Lavender;
+        }
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+            panelAppointment.BringToFront();
+        }
+
+        private void panelDentist_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void labelService_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ frmLogin/frmLogin.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace frmLogin
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ckDisplay = new System.Windows.Forms.CheckBox();
+            this.labelDentist = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(309, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(208, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(213, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(309, 154);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ckDisplay
+            // 
+            this.ckDisplay.AutoSize = true;
+            this.ckDisplay.Location = new System.Drawing.Point(309, 203);
+            this.ckDisplay.Name = "ckDisplay";
+            this.ckDisplay.Size = new System.Drawing.Size(134, 24);
+            this.ckDisplay.TabIndex = 5;
+            this.ckDisplay.Text = "Show password";
+            this.ckDisplay.UseVisualStyleBackColor = true;
+            // 
+            // labelDentist
+            // 
+            this.labelDentist.AutoSize = true;
+            this.labelDentist.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelDentist.Location = new System.Drawing.Point(354, 24);
+            this.labelDentist.Name = "labelDentist";
+            this.labelDentist.Size = new System.Drawing.Size(89, 37);
+            this.labelDentist.TabIndex = 31;
+            this.labelDentist.Text = "Login";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(818, 318);
+            this.Controls.Add(this.labelDentist);
+            this.Controls.Add(this.ckDisplay);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmLogin";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox ckDisplay;
+        private System.Windows.Forms.Label labelDentist;
+    }
+}
