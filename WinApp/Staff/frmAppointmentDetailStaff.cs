dfbf2331e429bd495f75a929a2c938931d1c9699
kien <masterkien1284@gmail.com>
2022-07-06 17:37:26 +0700

    finish

---
+++ WinApp/Staff/frmAppointmentDetailStaff.cs
@@ -0,0 +1,296 @@
+﻿using BusinessObject.Models;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinApp
+{
+    public partial class frmAppointmentDetailStaff : Form
+    {
+        public IAppointmentRepository appointmentRepository;
+        public ICustomerRepository customerRepository;
+        public IAppointmentServiceRepository appointmentServiceRepository;
+        public IServiceRepository serviceRepository;
+        public List<Service> detailList;
+        public Appointment appointment;
+        public bool InsertUpdateFlag;
+        public BindingSource BindingSource;
+        public frmAppointmentDetailStaff()
+        {
+            InitializeComponent();
+            serviceRepository = new ServiceRepository();
+            label5.Visible = false;
+        }
+
+        private void frmAppointmentDetailStaff_Load(object sender, EventArgs e)
+        {
+            comboBoxService.DataSource = serviceRepository.GetActiveServiceList();
+            comboBoxService.DisplayMember = "Name";
+            comboBoxService.ValueMember = "Id";
+            if (InsertUpdateFlag)
+            {
+                dateTimePickerTime.MinDate = DateTime.Now.Date.AddDays(1);
+                dateTimePickerTime.MaxDate = DateTime.Now.Date.AddDays(14);
+                dateTimePickerTime.Value = DateTime.Now.Date.AddDays(1);
+                LoadDetailList();
+                buttonCancel.Visible = false;
+            }
+            else
+            {
+                if (appointment.Time >= DateTime.Now.Date.AddDays(1) && appointment.Status == 1) UpdateAble();
+                else ViewOnly();
+                ChangeTimeOptionList();
+                dateTimePickerTime.Value = appointment.Time;
+                comboBoxWorkingHour.SelectedIndex = comboBoxWorkingHour.Items.IndexOf(appointment.WorkingHour);
+                textBoxPhone.Text = customerRepository.GetById(appointment.CustomerId).Phone;
+                LoadDetailList();
+            }
+        }
+
+        public void UpdateAble()
+        {
+            textBoxPhone.Enabled = false;
+        }
+
+        public void ViewOnly()
+        {
+            switch (appointment.Status)
+            {
+                case 1:
+                    {
+                        label5.Text = "This appointment is too late to update";
+                        label5.Visible = true;
+                        break;
+                    }
+                case 2:
+                    {
+                        label5.Text = "This appointment was done";
+                        label5.Visible = true;
+                        break;
+                    }
+                case 3:
+                    {
+                        label5.Text = "This appointment was cancelled";
+                        label5.Visible = true;
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+            buttonDeleteService.Visible = false;
+            buttonSave.Visible = false;
+            dateTimePickerTime.Enabled = false;
+            comboBoxService.Enabled = false;
+            comboBoxWorkingHour.Enabled = false;
+            textBoxPhone.Enabled = false;
+            buttonCancel.Visible = false;
+        }
+
+        public void LoadDetailList()
+        {
+            BindingSource = new BindingSource();
+            BindingSource.DataSource = detailList;
+            dataGridViewDetail.DataSource = BindingSource;
+            buttonDeleteService.Enabled = false;
+            if (dataGridViewDetail.Columns["ServiceTypeId"] != null)
+            {
+                dataGridViewDetail.Columns["ServiceTypeId"].Visible = false;
+            }
+            if (dataGridViewDetail.Columns["AppointmentServices"] != null)
+            {
+                dataGridViewDetail.Columns["AppointmentServices"].Visible = false;
+            }
+        }
+
+        private void comboBoxService_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            Service service = comboBoxService.SelectedItem as Service;
+            if (detailList.Count >= 3) {
+                MessageBox.Show("The maximum number of selected services is 3");
+                return;
+            } 
+            foreach(Service tmp in detailList){
+                if(tmp.Id == service.Id)
+                {
+                    MessageBox.Show("Service already choosed");
+                    return;
+                }
+            }
+            detailList.Add(service);
+            LoadDetailList();
+            ChangeTimeOptionList();
+        }
+
+        public void ChangeTimeOptionList()
+        {
+            comboBoxWorkingHour.Text = string.Empty;
+            if (!(detailList != null && detailList.Count != 0))
+            {
+                BindingSource = new BindingSource();
+                BindingSource.DataSource = new List<string>();
+                comboBoxWorkingHour.DataSource = BindingSource;
+                return;
+            }
+            BindingSource = new BindingSource();
+            BindingSource.DataSource = appointmentRepository.GetTimeOptionByDate(dateTimePickerTime.Value, detailList, appointment);
+            comboBoxWorkingHour.DataSource = BindingSource;
+        }
+
+        private void dataGridViewDetail_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            buttonDeleteService.Enabled = true;
+        }
+
+        private void dateTimePickerTime_ValueChanged(object sender, EventArgs e)
+        {
+            ChangeTimeOptionList();
+        }
+
+        private void buttonDeleteService_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridViewDetail.SelectedRows)
+            {
+                foreach (Service service in detailList)
+                {
+                    if (service.Id == int.Parse(row.Cells[0].Value.ToString()))
+                    {
+                        detailList.Remove(service);
+                        break;
+                    }
+                }
+            }
+            LoadDetailList();
+            ChangeTimeOptionList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Customer customer = customerRepository.CheckCustomerByPhone(textBoxPhone.Text);
+            if (customer == null)
+            {
+                MessageBox.Show("Customer with this phone does not exist\nCreate a profile for this customer before create an appointment");
+            }
+            else
+            {
+                frmCustomerDetailStaff frmCustomerDetailStaff = new frmCustomerDetailStaff()
+                {
+                    CustomerRepository = customerRepository,
+                    isInsert = false,
+                    Text = "Update new Customer",
+                    CustomerInfo = customer,
+                };
+                frmCustomerDetailStaff.ShowDialog();
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if(textBoxPhone.Text.Trim().Length != 10 || !int.TryParse(textBoxPhone.Text.Trim(), out _))
+            {
+                MessageBox.Show("Wrong phone format. Ex: 0123456789");
+                return;
+            }
+            Customer customer = customerRepository.CheckCustomerByPhone(textBoxPhone.Text.Trim());
+            if (customer == null)
+            {
+                MessageBox.Show("Customer with this phone does not exist\nCreate a profile for this customer before create an appointment");
+                return;
+            }
+            if (!(detailList != null && detailList.Count != 0))
+            {
+                MessageBox.Show("Service list must not be empty");
+                return;
+            }
+            if (!appointmentRepository.GetTimeOptionByDate(dateTimePickerTime.Value, detailList, this.appointment).Any(o => o.Equals(comboBoxWorkingHour.Text))) {
+                MessageBox.Show("This working hour slot is not available now\nPlease reload form or comeback later");
+                return;
+            };
+
+
+
+            
+            if (InsertUpdateFlag)
+            {
+                Appointment existingAppointment = appointmentRepository.GetWaitingAppointmentByCustomerId(customer.Id);
+                if (existingAppointment != null)
+                {
+                    MessageBox.Show("This customer already has a waiting appointment on " + existingAppointment.Time.Date.ToString().Split(" ")[0]);
+                    return;
+                }
+                Appointment appointment = new Appointment
+                {
+                    CustomerId = customer.Id,
+                    DentistId = appointmentRepository.GetGetSuitableDentistByDate(dateTimePickerTime.Value, detailList, comboBoxWorkingHour.Text, this.appointment),
+                    Time = dateTimePickerTime.Value,
+                    WorkingHour = comboBoxWorkingHour.Text,
+                    Status = 1
+                };
+                int id = appointmentRepository.AddAppointment(appointment).Id;
+                foreach (Service service in detailList)
+                {
+                    appointmentServiceRepository.AddAppointmentService(new AppointmentService
+                    {
+                        AppointmentId = id,
+                        ServiceId = service.Id
+                    });
+                }
+                MessageBox.Show("Added successfully", "Add appointment");
+            }
+            else
+            {
+                Appointment appointment = new Appointment
+                {
+                    Id = this.appointment.Id,
+                    CustomerId = customer.Id,
+                    DentistId = appointmentRepository.GetGetSuitableDentistByDate(dateTimePickerTime.Value, detailList, comboBoxWorkingHour.Text, this.appointment),
+                    Time = dateTimePickerTime.Value,
+                    WorkingHour = comboBoxWorkingHour.Text,
+                    Status = 1
+                };
+                appointmentRepository.UpdateAppointment(appointment);
+                appointmentServiceRepository.DeleteAppointmentServiceByAppointmentId(appointment.Id);
+                foreach (Service service in detailList)
+                {
+                    appointmentServiceRepository.AddAppointmentService(new AppointmentService
+                    {
+                        AppointmentId = appointment.Id,
+                        ServiceId = service.Id
+                    });
+                }
+                MessageBox.Show("Updated successfully", "Update appointment");
+            }
+            DialogResult = DialogResult.OK;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult d;
+                d = MessageBox.Show("Sure to cancel this appointment?", "Confirm deleting", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (d == DialogResult.Yes)
+                {
+                    appointment.Status = 3;
+                    appointmentRepository.UpdateAppointment(appointment);
+                    MessageBox.Show("Cancelled appointment successfully", "Cancel appointment");
+                    Close();
+                    DialogResult = DialogResult.OK;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Delete product error");
+            }
+        }
+    }
+}
---
+++ WinApp/Staff/frmHomeStaff.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessObject.Models;
+using WinApp;
+using DataAccess;
+namespace WinApp
+{
+    public partial class frmHomeStaff : Form
+    {
+        List<Panel> panelList = new List<Panel>();
+        public Account accountLogin { get; set; }
+
+        ICustomerRepository customerRepository;
+        IAppointmentRepository appointmentRepository;
+        IAccountRepository accountRepository;
+        IAppointmentServiceRepository appointmentServiceRepository;
+        IServiceRepository serviceRepository;
+        BindingSource source;
+        BindingSource sourceAppointment;
+
+        private static Customer customer;
+        public frmHomeStaff()
+        {
+            InitializeComponent();
+            customerRepository = new CustomerRepository();
+            appointmentRepository = new AppointmentRepository();
+            accountRepository = new AccountRepository();
+            appointmentServiceRepository = new AppointmentServiceRepository();
+            serviceRepository = new ServiceRepository();
+        }
+
+        private void LoadListCustomer(List<Customer> list)
+        {
+            List<CustomerChange> changeList = new List<CustomerChange>();
+            foreach (Customer customer in list)
+            {
+                CustomerChange change = new CustomerChange()
+                {
+                    Id = customer.Id,
+                    Name = customer.Name,
+                    Phone = customer.Phone,
+                    Gender = customer.Gender ?"Male":"Female",
+                    Dob = customer.Dob,
+                };
+                changeList.Add(change);
+            }
+            source = new BindingSource();
+            source.DataSource = changeList;
+            dataGridViewCustomer.DataSource = source;
+            customer = list[0];
+
+            if (dataGridViewCustomer.Columns["Appointments"] != null)
+            {
+                dataGridViewCustomer.Columns["Appointments"].Visible = false;
+            }
+
+            if (dataGridViewCustomer.Columns["Dob"] != null)
+            {
+                dataGridViewCustomer.Columns["Dob"].HeaderText = "Date of birth";
+            }
+
+        }
+
+        private void buttonAppointment_Click(object sender, EventArgs e)
+        {
+            panelCustomer.SendToBack();
+            panelAppointment.BringToFront();
+            buttonAppointment.BackColor = Color.LightBlue;
+            buttonCustomer.BackColor = Color.Lavender;
+        }
+
+        private void buttonCustomer_Click(object sender, EventArgs e)
+        {
+            panelCustomer.BringToFront();
+            panelAppointment.SendToBack();
+            buttonAppointment.BackColor = Color.Lavender;
+            buttonCustomer.BackColor = Color.LightBlue;
+            LoadListCustomer(customerRepository.GetAll());
+        }
+
+        private void buttonService_Click(object sender, EventArgs e)
+        {
+            panelAppointment.SendToBack();
+            panelCustomer.SendToBack();
+            buttonAppointment.BackColor = Color.Lavender;
+            buttonCustomer.BackColor = Color.Lavender;
+        }
+
+        private void buttonDentist_Click(object sender, EventArgs e)
+        {
+            panelAppointment.SendToBack();
+            panelCustomer.SendToBack();
+            buttonAppointment.BackColor = Color.Lavender;
+            buttonCustomer.BackColor = Color.Lavender;
+        }
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+            panelAppointment.BringToFront();
+            LoadAppointmentList();
+            LoadListCustomer(customerRepository.GetAll());
+            List<Account> dentistList = new List<Account>();
+            dentistList.Add(new Account { 
+                Id = 0,
+                Name = "All"
+            });
+            dentistList.AddRange(accountRepository.GetALLDentistList());
+            comboBoxAppointmentDentist.DataSource = dentistList;
+            comboBoxAppointmentDentist.DisplayMember = "Name";
+            comboBoxAppointmentDentist.ValueMember = "Id";
+            comboBoxStatus.SelectedIndex = 0;
+        }
+
+        private void buttonFindAppointment_Click(object sender, EventArgs e)
+        {
+            LoadAppointmentList();
+        }
+
+        public void LoadAppointmentList()
+        {
+            buttonAppointmentUpdate.Enabled = false;
+            int status;
+            switch (comboBoxStatus.Text)
+            {
+                case "All":
+                    {
+                        status = 0;
+                        break;
+                    }
+                case "Waiting":
+                    {
+                        status = 1;
+                        break;
+                    }
+                case "Done":
+                    {
+                        status = 2;
+                        break;
+                    }
+                case "Cancel":
+                    {
+                        status = 3;
+                        break;
+                    }
+                default:
+                    {
+                        status = 0;
+                        break;
+                    }
+            }
+            Account dentist = comboBoxAppointmentDentist.SelectedItem as Account;
+            int dentistId = dentist != null ? dentist.Id : 0;
+            dataGridViewAppointment.DataSource = appointmentRepository.GetAppointmentList(dateTimePickerAppointmentDate.Value, textBoxAppointmentPhone.Text, dentistId, status);
+            if (dataGridViewAppointment.Columns["CustomerId"] != null)
+            {
+                dataGridViewAppointment.Columns["CustomerId"].Visible = false;
+            }
+            if (dataGridViewAppointment.Columns["DentistId"] != null)
+            {
+                dataGridViewAppointment.Columns["DentistId"].Visible = false;
+            }
+            if (dataGridViewAppointment.Columns["AppointmentServices"] != null)
+            {
+                dataGridViewAppointment.Columns["AppointmentServices"].Visible = false;
+            }
+            if (dataGridViewAppointment.Columns["Status"] != null)
+            {
+                dataGridViewAppointment.Columns["Status"].Visible = false;
+            }
+        }
+
+        private void buttonAppointmentAdd_Click(object sender, EventArgs e)
+        {
+            frmAppointmentDetailStaff frmAppointmentDetailstaff = new frmAppointmentDetailStaff
+            {
+                appointmentRepository = appointmentRepository,
+                customerRepository = customerRepository,
+                appointmentServiceRepository = appointmentServiceRepository,
+                serviceRepository = serviceRepository,
+                InsertUpdateFlag = true,
+                detailList = new List<Service>()
+            };
+            if (frmAppointmentDetailstaff.ShowDialog() == DialogResult.OK)
+            {
+                LoadAppointmentList();
+            }
+        }
+
+        private void buttonAppointmentUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(dataGridViewAppointment.SelectedRows[0].Cells[0].Value.ToString());
+            Appointment appointment = appointmentRepository.GetAppointmentById(id);
+            frmAppointmentDetailStaff frmAppointmentDetailstaff = new frmAppointmentDetailStaff
+            {
+                appointmentRepository = appointmentRepository,
+                customerRepository = customerRepository,
+                appointment = appointment,
+                appointmentServiceRepository = appointmentServiceRepository,
+                InsertUpdateFlag = false,
+                serviceRepository = serviceRepository,
+                detailList = serviceRepository.GetServiceListByAppointmentId(id),
+            };
+            if (frmAppointmentDetailstaff.ShowDialog() == DialogResult.OK)
+            {
+                LoadAppointmentList();
+            }
+        }
+        private void buttonCustomerAdd_Click(object sender, EventArgs e)
+        {
+            frmCustomerDetailStaff frmCustomerDetailStaff = new frmCustomerDetailStaff()
+            {
+                CustomerRepository = customerRepository,
+                isInsert = true,
+                Text = "Add new Customer",
+            };
+            if (frmCustomerDetailStaff.ShowDialog() == DialogResult.OK)
+            {
+                LoadListCustomer(customerRepository.GetAll());
+            }
+        }
+
+        private void buttonCustomerUpdate_Click(object sender, EventArgs e)
+        {
+            frmCustomerDetailStaff frmCustomerDetailStaff = new frmCustomerDetailStaff()
+            {
+                CustomerRepository = customerRepository,
+                isInsert = false,
+                Text = "Update new Customer",
+                CustomerInfo = customer,
+            };
+            if (frmCustomerDetailStaff.ShowDialog() == DialogResult.OK)
+            {
+                LoadListCustomer(customerRepository.GetAll());
+            }
+        }
+
+        private void dataGridViewCustomer_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int id = int.Parse(dataGridViewCustomer.Rows[e.RowIndex].Cells["Id"].FormattedValue.ToString());
+
+            customer = customerRepository.GetById(id);
+        }
+
+        private void dataGridViewCustomer_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int id = int.Parse(dataGridViewCustomer.Rows[e.RowIndex].Cells["Id"].FormattedValue.ToString());
+
+            customer = customerRepository.GetById(id);
+        }
+
+     
+
+        private void buttonCustomerFind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = textBoxCustomerName.Text.Trim();
+                string phone = textBoxCustomerPhone.Text.Trim();
+                if (name.Length != 0 && phone.Length != 0)
+                {
+                    LoadListCustomer(customerRepository.SearchCustomerByNameAndPhone(name, phone));
+                }
+                else
+                {
+                    if (phone.Length != 0)
+                    {
+                        LoadListCustomer(customerRepository.SearchCustomerByPhone(phone));
+
+                    }
+                    else if (name.Length != 0)
+                    {
+                        LoadListCustomer(customerRepository.SearchCustomerByName(name));
+                    } else
+                    {
+                        LoadListCustomer(customerRepository.GetAll());
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Search customer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void txtLinkLabelNameAccountLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmProfile frmProfile = new frmProfile()
+            {
+                accountInfo = accountLogin,
+                AccountLogin = accountLogin,
+            };
+            frmProfile.ShowDialog();
+        }
+
+
+        private void dataGridViewCustomer_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int id = int.Parse(dataGridViewCustomer.Rows[e.RowIndex].Cells["Id"].FormattedValue.ToString());
+
+            customer = customerRepository.GetById(id);
+            buttonCustomerUpdate_Click(sender, e);
+        }
+
+        private void dataGridViewAppointment_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            buttonAppointmentUpdate.Enabled = true;
+        }
+    }
+}
+
+public class CustomerChange
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public string Gender{ get; set; }
+    public DateTime Dob { get; set; }
+}
